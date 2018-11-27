5547fb03aaa150bc9aed69c8976219a1bcb66313
Kah Yap <kahyap13@gmail.com>
2018-11-27 18:02:21 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,6 @@
 class Hash
   def keys_of(arguments)
     # code goes here
-    # let(:animals) { {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
-    return arguments
+
   end
 end
