fb9a6751f9e846642148130ddd693e8f8e0034d5
Kah Yap <kahyap13@gmail.com>
2018-11-29 23:39:15 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,19 @@
+require 'pry'
+# let(:animals) { {"sugar glider"=>"Australia","aye-aye"=> "Madagascar",
+# "red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
 class Hash
+
   def keys_of(arguments)
     # code goes here
+    keyName = []
+      animals.each do |animal|
+        animal.each do |k, v|
+          if v == arguments
+            keyName << k
+          end
+        end
+      end
+    return keyName
   end
+
 end
